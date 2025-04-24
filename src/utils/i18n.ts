03ef1f32384d2f5836aee4468b430dd1ca6060ea
tao-isaman <towlovelove@gmail.com>
2025-04-24 19:17:16 +0700

    fix i18n on build mode

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -2,27 +2,29 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import HttpBackend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import { InitOptions } from 'i18next';
+
+const i18nConfig: InitOptions = {
+  fallbackLng: 'en',
+  supportedLngs: ['en', 'th'],
+  defaultNS: 'common',
+  fallbackNS: 'common',
+  backend: {
+    loadPath: '/locales/{{lng}}/{{ns}}.json',
+  },
+  detection: {
+    order: ['path', 'cookie', 'navigator'],
+    caches: ['cookie']
+  },
+  interpolation: {
+    escapeValue: false,
+  },
+};
 
 i18n
   .use(HttpBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
-  .init({
-    fallbackLng: 'en',
-    supportedLngs: ['en', 'th'],
-    defaultNS: 'common',
-    fallbackNS: 'common',
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
-    },
-    detection: {
-      order: ['path', 'cookie', 'navigator'],
-      caches: ['cookie'],
-      cookieSameSite: 'strict',
-    },
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+  .init(i18nConfig);
 
 export default i18n; 
